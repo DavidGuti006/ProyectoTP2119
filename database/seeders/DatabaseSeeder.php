987ad739442ba6c08ee49d88f8e68e1e6315b844
David006 <davidgutierrezrozo@gmail.com>
2023-10-24 18:07:55 -0500

    Commit sin errores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Priorities;
 use App\Models\Status;
 use Illuminate\Database\Seeder;
 
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(Status::class);
+        $this->call(Priorities::class);
     }
 }
